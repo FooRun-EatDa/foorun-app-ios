11c83580816e8d8905951b8283a0d3fd5694b136
lgvv <lgw9898@naver.com>
2022-07-10 15:05:34 +0900

    [추가] 초기 프로젝트를 세팅

---
+++ Foorun/Foorun/SceneDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  SceneDelegate.swift
+//  Foorun
+//
+//  Created by Hamlit Jason on 2022/07/10.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+            
+            guard let windowScene = (scene as? UIWindowScene) else { return }
+            
+            window = UIWindow(windowScene: windowScene)
+            window?.backgroundColor = .systemBackground
+            window?.rootViewController = UINavigationController(rootViewController: ViewController())
+            window?.makeKeyAndVisible()
+        }
+}
+
---
+++ Foorun/Foorun/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Foorun
+//
+//  Created by Hamlit Jason on 2022/07/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
