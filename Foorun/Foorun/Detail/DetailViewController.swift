1ad665abb95a745fa208af1a1d63bb8118a291aa
heejin342 <tktmzp0526@naver.com>
2022-07-10 16:14:28 +0900

    [생성] Detail 파일 추가

---
+++ Foorun/Foorun/Detail/DetailViewController.swift
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.swift
+//  Foorun
+//
+//  Created by 김희진 on 2022/07/10.
+//
+
+import Foundation
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+}
