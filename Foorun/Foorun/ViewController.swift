5c97dbab715bf482a8d1ae35c4ae403ed30f5043
김지훈 <ben224@naver.com>
2022-07-31 16:52:19 +0900

    [추가] 좋아요한 맛집 페이지 추가

--- Foorun/Foorun/ViewController.swift
+++ Foorun/Foorun/ViewController.swift
@@ -11,9 +11,30 @@ import Logger
 
 class ViewController: UIViewController {
 
+    private lazy var button: UIButton = {
+        let button = UIButton()
+        button.setTitle("GO!", for: .normal)
+        button.backgroundColor = .black
+        button.addTarget(self, action: #selector(openBookMarkPage), for: .touchUpInside)
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        view.addSubview(button)
+        button.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+            make.width.height.equalTo(100)
+        }
+    }
+    
+    @objc func openBookMarkPage() {
+        let bookMarkViewController = BookMarkViewController()
+
+        let nav = UINavigationController(rootViewController: bookMarkViewController)
+        nav.modalPresentationStyle = .overFullScreen
+        present(nav, animated: false, completion: nil)
     }
 }
 
