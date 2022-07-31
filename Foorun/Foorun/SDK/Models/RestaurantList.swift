2e4dc1a86a732e009cc04c7051167a5cc2c89fb9
heejin342 <tktmzp0526@naver.com>
2022-07-31 16:40:26 +0900

    [추가] API request 함수 추가

---
+++ Foorun/Foorun/SDK/Models/RestaurantList.swift
@@ -0,0 +1,18 @@
+//
+//  RestaurantList.swift
+//  Foorun
+//
+//  Created by 김희진 on 2022/07/31.
+//
+
+import Foundation
+
+struct RestaurantList: Codable {
+    let id: Int?
+    let name: String?
+    let explanation: String?
+    let imgUrl: String?
+    let distance: Double?
+    let hashTags: [String]
+    let liked: Bool
+}
