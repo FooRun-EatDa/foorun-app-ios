eaae868b38fc4a389e54b6b35098e015321328d2
lgvv <lgw9898@naver.com>
2022-08-07 16:35:06 +0900

    [추가] 인증하기 화면 추가

--- Foorun/Foorun/Setting/Views/CertificationView.swift
+++ Foorun/Foorun/Setting/Views/CertificationView.swift
@@ -8,9 +8,52 @@
 import SwiftUI
 
 struct CertificationView: View {
+    @Environment(\.presentationMode) var mode: Binding<PresentationMode>
+    @Environment(\.editMode) private var editMode
+    
+    
+    @State private var disableTextField = false
+    @State var email: String = ""
+    @State var code: String = ""
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            
+            Spacer()
+            
+            TextField("이메일을 입력해주세요.", text: $email)
+                .padding()
+                .textFieldStyle(.roundedBorder)
+                .disabled(disableTextField)
+            
+            if !disableTextField {
+                Button {
+                    disableTextField.toggle()
+                    // 액션
+                } label: {
+                    Text("이메일 인증")
+                }
+            }
+            
+            if disableTextField {
+                TextField("인증코드를 입력해주세요.", text: $code)
+                    .padding()
+                    .textFieldStyle(.roundedBorder)
+                
+                Button {
+                    // APIRequest
+                    self.mode.wrappedValue.dismiss()
+                } label: {
+                    Text("인증코드 입력")
+                }
+            }
+            
+            Spacer()
+            
+            
+        }
+        .background(Color(uiColor: .secondarySystemBackground))
+        
     }
 }
 
