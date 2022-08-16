9bb83fb823f1e1c1be11986ff5db5802e45ebec7
SeYeongYoon <ssyoun4092@naver.com>
2022-08-16 19:03:45 +0900

    [삭제] 불필요한 코드 삭제

--- Foorun/Foorun/Extensions/UIView.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-import UIKit
-
-extension UIView {
-    func addSubviews(_ views: [UIView]) {
-        views.forEach { self.addSubview($0) }
-    }
-}
--- Foorun/Foorun/Network/Service/RequestNeeds.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-protocol RequestNeeds {
-    var baseURL: URL { get }
-    var path: String { get }
-    var httpMethod: HTTPMethod { get }
-}
