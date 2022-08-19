28a2af09cc1a6c0d18d12c5366c01ffcbff12cf8
김지훈 <ben224@naver.com>
2022-08-19 15:39:31 +0900

    [추가] 탭바 생성

---
+++ Foorun/Foorun/TabBarController.swift
@@ -0,0 +1,48 @@
+import UIKit
+import SwiftUI
+class TabBarController: UITabBarController {
+    private lazy var mapViewContoller: UIViewController = {
+        let viewController = UINavigationController(rootViewController: MapViewController())
+        
+        viewController.tabBarItem = UITabBarItem(
+            title: "홈",
+            image: UIImage(named: "tabBarItemHome"),
+            tag: 1
+        )
+        
+        return viewController
+    }()
+    
+    private lazy var bookmarkViewController: UIViewController = {
+        let viewController = UINavigationController(rootViewController: BookmarkViewController())
+        
+        viewController.tabBarItem = UITabBarItem(
+            title: "북마크",
+            image: UIImage(named: "tabBarItemBookmark"),
+            tag: 1
+        )
+        
+        return viewController
+    }()
+    
+    private lazy var settingViewController: UIViewController = {
+        let settingViewController = UIHostingController(rootView: SettingView())
+        let viewController = UINavigationController(rootViewController: settingViewController)
+        
+        viewController.tabBarItem = UITabBarItem(
+            title: "마이",
+            image: UIImage(named: "tabBarItemMy"),
+            tag: 1
+        )
+        
+        return viewController
+    }()
+    
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+                
+        viewControllers = [mapViewContoller, bookmarkViewController, settingViewController]
+    }
+}
