b6149ae1d6691bdd83bcc2a8dd3e8047214adaa2
SeYeongYoon <ssyoun4092@naver.com>
2022-08-19 21:09:30 +0900

    [추가] 탭바 Asset 추가

---
+++ Foorun/Foorun/Event/Detail/EventValid.swift
@@ -0,0 +1,13 @@
+//
+//  EventValid.swift
+//  Foorun
+//
+//  Created by MacPro on 2022/08/19.
+//
+
+import Foundation
+
+struct EventValid: Decodable {
+    let content: String
+    let status: Int
+}
