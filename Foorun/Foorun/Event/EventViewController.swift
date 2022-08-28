0f72d6f45108534e6f177abc4e7acde07cb005da
SeYeongYoon <ssyoun4092@naver.com>
2022-08-28 13:28:29 +0900

    [수정] 더미데이터 수정

--- Foorun/Foorun/Event/EventViewController.swift
+++ Foorun/Foorun/Event/EventViewController.swift
@@ -13,7 +13,7 @@ class EventViewController: UIViewController {
 
     // MARK: - Properties
     
-    var events: [Event] = Event.dummyModel {
+    var events: [Event] = [] {
         didSet {
             updateEvents()
         }
@@ -34,7 +34,7 @@ class EventViewController: UIViewController {
         setupNavigationBar()
         setupViews()
         updateEvents()
-//        fetchEvents(page: 0)
+        fetchEvents(page: 0)
     }
 
     override func viewWillAppear(_ animated: Bool) {
