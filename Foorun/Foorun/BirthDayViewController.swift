917687141fa36ff9a420bb1f93dde1f1043ab6a0
lgvv <lgw9898@naver.com>
2022-09-03 17:01:53 +0900

    🎉 HAPPY BIRTHDAY!

---
+++ Foorun/Foorun/BirthDayViewController.swift
@@ -0,0 +1,16 @@
+//
+//  BirthDayViewController.swift
+//  Foorun
+//
+//  Created by Hamlit Jason on 2022/09/03.
+//
+
+import UIKit
+import SnapKit
+
+class BirthDayViewContoller: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
