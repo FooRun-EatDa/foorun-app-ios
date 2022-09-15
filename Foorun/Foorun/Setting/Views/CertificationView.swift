2c02284dd0a5c8a696dc23a6c130bb7289935a58
김지훈 <ben224@naver.com>
2022-09-16 00:58:37 +0900

    [수정] 인증로직 수정
    alert 추가
    
    이메일을 입력하지 않았을 경우
    이메일이 유효하지 않은 경우 (@khu.ac.kr 포함?)
    인증코드가 올바르지 않은 경우
    인증을 성공했을 경우

--- Foorun/Foorun/Setting/Views/CertificationView.swift
+++ Foorun/Foorun/Setting/Views/CertificationView.swift
@@ -8,10 +8,16 @@
 import SwiftUI
 import FoorunKey
 
+enum ErrorMessage: String {
+    case none = "인증 성공 !"
+    case empty = "이메일을 입력해주세요"
+    case incorrectCode = "인증번호가 올바르지 않아요"
+    case invalidEmail = "이메일이 올바르지 않아요"
+}
+
 struct CertificationView: View {
     @Environment(\.presentationMode) var mode: Binding<PresentationMode>
     @Environment(\.editMode) private var editMode
-//    @EnvironmentObject private var viewModel: SettingViewModel
     
     @State private var disabledEmailField = false
     @State var email: String = ""
@@ -20,6 +26,10 @@ struct CertificationView: View {
     
     /// 서버에서 내려온 인증 코드
     @State var certificationCode: String = ""
+    
+    /// 유효하지 않은 이메일 Alert
+    @State private var showingAlert = false
+    @State private var errorMessage: ErrorMessage = .empty
     
     var body: some View {
         VStack {
@@ -31,17 +41,27 @@ struct CertificationView: View {
                 .padding()
                 .textFieldStyle(.roundedBorder)
                 .disabled(disabledEmailField)
-                
+            
             if !disabledEmailField {
                 Button {
-                    if !email.isEmpty {
-                        disabledEmailField.toggle()
-                        didTap이메일_인증_요청()
+                    if email.isEmpty {
+                        errorMessage = .empty
+                        showingAlert = true
+                    } else {
+                        if !email.contains("@khu.ac.kr") {
+                            errorMessage = .invalidEmail
+                            showingAlert = true
+                        } else {
+                            disabledEmailField.toggle()
+                            didTap이메일_인증_요청()
+                        }
                     }
                     
                 } label: {
                     Text("이메일 인증")
                         .foregroundColor(.orange)
+                } .alert(isPresented: $showingAlert) {
+                    Alert(title: Text(errorMessage.rawValue))
                 }
                 
             }
@@ -63,17 +83,32 @@ struct CertificationView: View {
                      
                      
                      """)
-                    .foregroundColor(.red)
-                    .font(.caption)
-                    .frame(alignment: .leading)
+                .foregroundColor(.red)
+                .font(.caption)
+                .frame(alignment: .leading)
                 
                 Button {
-                    // APIRequest
-                    self.didTap인증()
+                    didTap인증 { isSuccess in
+                        if isSuccess {
+                            NotificationCenter.default.post(name: NSNotification.Name(rawValue: "verificationCompleted") , object: nil)
+                            errorMessage = .none
+                            showingAlert = true
+                        } else {
+                            errorMessage = .incorrectCode
+                            showingAlert = true
+                        }
+                    }
                 } label: {
                     Text("✌️ 인증하기 ✌️")
                         .foregroundColor(.orange)
-                }   
+                } .alert(errorMessage.rawValue, isPresented: $showingAlert) {
+                    Button("OK", role: .cancel) {
+                        if errorMessage == .none {
+                            mode.wrappedValue.dismiss()
+                        }
+                    }
+                
+                }
             }
             
             Spacer()
@@ -81,7 +116,7 @@ struct CertificationView: View {
             Text("""
                 이메일을 잘못 입력하신 경우, 페이지를 나갔다 들어와주세요:)
                 
-
+                
                 """)
             .font(.caption)
             
@@ -96,28 +131,36 @@ struct CertificationView: View {
             method: .get,
             parameters: ["email": email]
         ).fetch { apiResponse in
-            print("apiResponse \(apiResponse) lgw9898@naver.com")
+            print("apiResponse: \(apiResponse), email: \(email)")
             guard let _ = apiResponse.data else { return }
         }
     }
     
-    private func didTap인증() {
+    private func didTap인증(completion: @escaping ((Bool) -> Void)) {
         API<Int>(
             requestString: "\(FoorunRequest.Auth.auth)?email=\(email)&verificationCode=\(code)",
             method: .post,
             parameters: [:]
-        ).fetch { apiResponse in
-            guard let code = apiResponse.data else { return }
-            // NOTE: - 성공해야만 data가 내려옵니다.
-            
-            UserDefaultManager.shared.token = "\(code)"
-            
-            NotificationCenter.default.post(name: NSNotification.Name(rawValue: "verificationCompleted") , object: nil)
-            
-            self.mode.wrappedValue.dismiss()
+        ).fetchResult { result in
+            print(result)
+            switch result {
+                
+            case .success(let apiResponse):
+                guard let code = apiResponse.data else {
+                    errorMessage = .incorrectCode
+                    print(errorMessage, "asd")
+                    showingAlert = true
+                    return
+                }
+                // NOTE: - 성공해야만 data가 내려옵니다.
+                UserDefaultManager.shared.token = "\(code)"
+                completion(true)
+
+            case .failure(let error):
+                completion(false)
+                print("🚨Error:: \(error.localizedDescription) ~메소드 에러 발생!")
+            }
         }
-        
-        
     }
 }
 
